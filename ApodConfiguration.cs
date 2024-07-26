966ad5ebe8c69ce4805a5e10a661f267e6383325
George wanjohi <gwanjohim@gmail.com>
2024-07-26 20:17:48 +0300

    ADD: Configuration class -  This will be used to "strongly type" the settings information.

---
+++ ApodConfiguration.cs
@@ -0,0 +1,8 @@
+namespace Senri_APOD_Wanjohi;
+
+public class ApodConfiguration
+{
+    public string ApiBaseUrl { get; set; } = "";
+    public string ApiKey { get; set; } = "";
+    public string Date { get; set; } = "";
+}
