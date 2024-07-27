8c460a153767766499ce2d7c70666c69cabca5f1
George wanjohi <gwanjohim@gmail.com>
2024-07-27 15:50:18 +0300

    ADD: Code to:
    a. Load configuration from appsettings.json file
    
    b. pull image from the APOD api
    
    c. Save image to a file

--- Program.cs
+++ Program.cs
@@ -1,3 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Net;
+using System.Text.Json;
+using RestSharp;
+using Senri_APOD_Wanjohi;
+
+ApodConfiguration LoadConfiguration()
+{
+    if (!File.Exists("AppSettings.json"))
+    {
+        throw new Exception("The Settings file cannot be found");
+    }
+
+    using StreamReader r = new StreamReader("AppSettings.json");
+    string json = r.ReadToEnd();
+    var config = JsonSerializer.Deserialize<ApodConfiguration>(json);
+    return config!;
+}
+
+APODAPIResult? PullImage(ApodConfiguration config)
+{
+    var client = new RestClient(config.ApiBaseUrl);
+    var request = new RestRequest("", Method.Get);
+    request.AddParameter("api_key", config.ApiKey);
+    request.AddParameter("date", config.Date);
+    var response = client.Execute(request);
+    if (response.Content != null)
+    {
+        var apodApiResult = JsonSerializer.Deserialize<APODAPIResult>(response.Content);
+        return apodApiResult;
+    }
+    return null;
+}
+
+
+void SaveImageOfTheDay(APODAPIResult? imageData, string downloadDirectory)
+{
+    if (imageData != default)
+    {
+        var client = new RestClient(imageData.url);
+        var request = new RestRequest("", Method.Get);
+        byte[] response = client.DownloadData(request);
+        var filePath = $"{downloadDirectory}/{imageData.title}";
+
+        if (!Directory.Exists(downloadDirectory))
+        {
+            Directory.CreateDirectory(downloadDirectory);
+            var stream = File.Create(filePath);
+            stream.Write(response);
+            stream.Close();
+        }
+        else
+        {
+            if(File.Exists(filePath))
+                return;
+            var stream = File.Create(filePath);
+            stream.Write(response);
+            stream.Close();
+        }
+    }
+    else
+    {
+        Console.WriteLine("The image does not exist");
+    }
+}
+
+var apodConfiguration = LoadConfiguration();
+var imageResult = PullImage(apodConfiguration);
+SaveImageOfTheDay(imageResult, apodConfiguration.DownloadLocation);
+
+
