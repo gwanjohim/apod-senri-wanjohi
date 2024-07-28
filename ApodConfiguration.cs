94b9acc271b7a683ebb14fb3d42455c67fae29bb
George wanjohi <gwanjohim@gmail.com>
2024-07-28 20:25:35 +0300

    Make apod api result class properties nullable

--- ApodConfiguration.cs
+++ ApodConfiguration.cs
@@ -11,11 +11,11 @@ public class ApodConfiguration
 }
 public class APODAPIResult
 {
-    public DateOnly date { get; set; }
-    public string explanation { get; set; }
-    public string hdurl { get; set; }
-    public string media_type { get; set; }
-    public string service_version { get; set; }
-    public string title { get; set; }
-    public string url { get; set; }
+    public DateOnly? date { get; set; }
+    public string? explanation { get; set; } 
+    public string? hdurl { get; set; }
+    public string? media_type { get; set; }
+    public string? service_version { get; set; }
+    public string? title { get; set; }
+    public string? url { get; set; }
 }
